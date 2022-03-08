44adb4df1b06f47a1143e855d1078b61217b376d
MHuesler <hueslermaxim@gmail.com>
2022-03-08 23:28:16 +0100

    minor

--- src/layouts/Docs/Docs.Layout.tsx
+++ src/layouts/Docs/Docs.Layout.tsx
@@ -31,7 +31,7 @@ const navItems = [
         ]
     },
     {
-        title: "Patients",
+        title: "Appointments",
         description: "Manage patients easily.",
         features: [
             {
